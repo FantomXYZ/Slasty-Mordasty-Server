e09fad18fbb2f34963260f6d79d17ee9500e4b58
Fantom_X_ <panchuk.dmitriy.04@mail.ru>
2023-03-23 13:16:32 +0300

    changes order entity

---
+++ Server/src/main/java/by/fpmibsu/slastymordasty/dao/Main.java
@@ -0,0 +1,23 @@
+import by.fpmibsu.PCBuilder.entity.component.Motherboard;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/pcbuilder", "root", "root");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String login = resultSet.getString("login");
+            String password = resultSet.getString("password");
+            System.out.println(id + " " + login + " " + password);
+        }
+    }
+}

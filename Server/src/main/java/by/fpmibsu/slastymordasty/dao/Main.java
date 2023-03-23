2573ea322f2dad90f142b64dce7afdeb8ee61cef
Fantom_X_ <panchuk.dmitriy.04@mail.ru>
2023-03-23 13:28:25 +0300

    changes order entity

--- Server/src/main/java/by/fpmibsu/slastymordasty/dao/Main.java
+++ Server/src/main/java/by/fpmibsu/slastymordasty/dao/Main.java
@@ -7,16 +7,16 @@ public class Main {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/pcbuilder", "root", "root");
+                "jdbc:mysql://localhost:3306/?user=root", "root", "root");
 
         Statement statement = connection.createStatement();
 
         ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
 
         while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String login = resultSet.getString("login");
-            String password = resultSet.getString("password");
+            int id = resultSet.getInt("ID");
+            String name = resultSet.getString("NAME");
+            String password = resultSet.getString("LOGIN");
             System.out.println(id + " " + login + " " + password);
         }
     }

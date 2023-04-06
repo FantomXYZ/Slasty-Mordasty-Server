85a9efde0c98b6f872396a80d55bf2a977e5f8c4
Fantom_X_ <panchuk.dmitriy.04@mail.ru>
2023-04-06 11:58:57 +0300

    Start Dao

---
+++ Server/src/java/by/fpmibsu/slastymordasty/dao/DaoException.java
@@ -0,0 +1,15 @@
+package java.by.fpmibsu.slastymordasty.dao;
+
+public class DaoException extends Exception {
+    public DaoException() {
+    }
+    public DaoException(String message) {
+        super(message);
+    }
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+}

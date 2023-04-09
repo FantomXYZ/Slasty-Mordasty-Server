4cca58ce8e442f7daa1d176fbeaa5c9605998af8
Fantom_X_ <panchuk.dmitriy.04@mail.ru>
2023-04-09 10:42:54 +0300

    Maven

--- Server/src/java/by/fpmibsu/slastymordasty/entity/Cake.java
+++
@@ -1,8 +0,0 @@
-package java.by.fpmibsu.slastymordasty.entity;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Cake extends Dessert implements Item{
-    private float radius;
-}
